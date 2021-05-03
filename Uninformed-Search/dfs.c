80b1cc27c5ef8c39a1747950d69e4144ad7b3b79
brunosampaio08 <bruno.sampaio08@unifesp.br>
2021-05-03 17:38:07 -0300

    wrong 15

--- Uninformed-Search/dfs.c
+++ Uninformed-Search/dfs.c
@@ -1,5 +1,6 @@
 /*
     URI Judge Problem No 3247 - Robots on a Grid
+    Bruno Sampaio Leite
 */
 
 #include <stdio.h>
@@ -10,63 +11,61 @@ enum color {white, grey, black};
 typedef struct boardT{
     int walkable;
     int tileColor;
+    unsigned long long int nodePathNum;
 }boardT;
 
-unsigned long int pathNum;
+unsigned long long int pathNum;
+int theGameLies;
 
 /* 
-    Basic traverse traverse's the board walking only down and right
-    Increments pathNum whenever a new path from s to t is found
+    Basic traverse traverse's the board walking only down and right;
+    It keeps a table containing the number of found paths on an already explored vertex, so that
+    when that vertex is found again through a new path it's not explored redundantly;
+    This approach is similar to Dynamic Programming
 */
 int basicTraverse(boardT **board, int currCol, int currRow, int size){
-    int rightPath = 0, downPath = 0;
     //printf("%d;%d\n", currCol, currRow);
+    int currPathNum = 0;
 
     if(currCol == size-1 && currRow == size-1){
-        pathNum++;
         return 1;
     }else{
+        board[currCol][currRow].tileColor = grey;
+
         if(currCol+1 < size &&
-            board[currCol+1][currRow].walkable == 1 && 
+            board[currCol+1][currRow].walkable == 1 &&
                 board[currCol+1][currRow].tileColor == white){
-                    downPath = basicTraverse(board, currCol+1, currRow, size);
-        }else if(currCol+1 < size && board[currCol+1][currRow].tileColor == grey){
-            pathNum++;
-            downPath = 1;
+                    currPathNum += basicTraverse(board, currCol+1, currRow, size);
+        }else if(currCol+1 < size && board[currCol+1][currRow].tileColor == black){
+            currPathNum += board[currCol+1][currRow].nodePathNum;
         }
 
         if(currRow+1 < size &&
-            board[currCol][currRow+1].walkable == 1 && 
+            board[currCol][currRow+1].walkable == 1 &&
                 board[currCol][currRow+1].tileColor == white){
-                    rightPath = basicTraverse(board, currCol, currRow+1, size);
-        }else if(currRow+1 < size && board[currCol][currRow+1].tileColor == grey){
-            pathNum++;
-            rightPath = 1;
+                    currPathNum += basicTraverse(board, currCol, currRow+1, size);
+        }else if(currRow+1 < size && board[currCol][currRow+1].tileColor == black){
+            currPathNum += board[currCol][currRow+1].nodePathNum;
         }
 
-        if(rightPath == 1 || downPath == 1){
-            board[currCol][currRow].tileColor = grey;
-        }else{
-            board[currCol][currRow].tileColor = black;
-        }
-    
+        board[currCol][currRow].tileColor = black;
+        board[currCol][currRow].nodePathNum = currPathNum;
     }
-    
-    return (rightPath || downPath);
+
+    return currPathNum;
 }
 
 /* 
-    Incremented traverse traverse's the board walking up, down, left and right
-    Return 1 when path is found, returns -1 if no path is found
+    Incremented traverse traverse's the board walking up, down, left and right;
+    Sets theGameLies to 1 if a path is found;
+    Simple DFS algorithm;
 */
 void incrementedTraverse(boardT **board, int currCol, int currRow, int size){
     //printf("%d;%d\n", currCol, currRow);
 
     if(currCol == size-1 && currRow == size-1){
-        printf("THE GAME IS A LIE\n");
-        pathNum = -1;
+        theGameLies = 1;
     }else{
-
         board[currCol][currRow].tileColor = grey;
 
         if(currCol+1 < size &&
@@ -103,7 +102,9 @@ void incrementedTraverse(boardT **board, int currCol, int currRow, int size){
 int main(){
     int size, i, j;
     char tile;
-    boardT **board; 
+    boardT **board;
+
+    pathNum = 0; theGameLies = 0;
     
     scanf("%d", &size);
 
@@ -118,6 +119,7 @@ int main(){
             tile = getchar();
             //tile = getchar();
             board[i][j].tileColor = white;
+            board[i][j].nodePathNum = 0;
             if(tile == '.'){
                 board[i][j].walkable = 1;
             }else if(tile == '#'){
@@ -131,8 +133,8 @@ int main(){
 
     basicTraverse(board, 0, 0, size);
 
-    if(pathNum != 0){
-        printf("%ld\n", pathNum%2147483647);
+    if(board[0][0].nodePathNum != 0){
+        printf("%lld\n", board[0][0].nodePathNum%2147483647);
     }else{
         for(i = 0; i < size; i++){
             for(j = 0; j < size; j++){
@@ -140,11 +142,11 @@ int main(){
             }
         }
         incrementedTraverse(board, 0, 0, size);
-        if(pathNum != -1){
+        if(theGameLies == 1){
+            printf("THE GAME IS A LIE\n");
+        }else{
             printf("INCONCEIVABLE\n");
         }
     }
-    
-
     return 0;
 }
